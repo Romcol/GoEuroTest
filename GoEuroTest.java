5d415a28e5aa00ac076e93b65beb11dbe90b5f2e
Colombat Romain <romain.colombat@insa-rennes.fr>
2016-01-25 19:24:08 +0100

    Small change in main

--- GoEuroTest.java
+++ GoEuroTest.java
@@ -11,7 +11,7 @@ public class GoEuroTest {
 		System.exit(1);
 	}
 	
-	public static void main (String[] args) throws Exception{
+	public static void main(String[] args){
 		
 		if(args.length != 1)
 		{
